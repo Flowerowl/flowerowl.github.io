828ff54d20ace6c383dd65a9a36354af9ca16fed
flowerowl <lazynightz@gmail.com>
2023-02-20 16:05:48 +0800

    update

--- heatmap/run.sh
+++ heatmap/run.sh
@@ -1,3 +1,1 @@
-#!/bin/bash
-
-python heatmap.py -p photo-points -o photos.png --width 1000 --osm -B 0.8 --osm_base http://b.tile.stamen.com/toner
+ss python heatmap.py -p photo-points -o photos.png --width 3000 --margin 500 --radius 20 --osm -B 1 --osm_base http://b.tile.stamen.com/toner
